d94874b1456d2e7ccf810a4d4ec5b4b740c77d30
Marcus Maciel <mendesxx.ti@hotmail.com>
2020-03-02 21:29:59 -0300

    Arquivos Iniciais

---
+++ src/main/java/com/example/zerotohero/ZerotoheroApplication.java
@@ -0,0 +1,13 @@
+package com.example.zerotohero;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZerotoheroApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZerotoheroApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/zerotohero/ZerotoheroApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.zerotohero;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZerotoheroApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
